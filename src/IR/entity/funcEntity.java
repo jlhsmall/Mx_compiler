f2bac957a19906b3536489a1e3365747699e60f5
jlhsmall <519323064@qq.com>
2021-03-24 21:27:11 +0800

    qwq

--- src/IR/entity/funcEntity.java
+++ src/IR/entity/funcEntity.java
@@ -2,18 +2,29 @@ package IR.entity;
 
 import IR.IRType.IRType;
 
+import java.util.ArrayList;
+
 /**
  * @author Jlhsmall
  * @date 2021/3/21 20:28
  */
 public class funcEntity extends Entity{
     public String name;
+    public ArrayList<Entity> paras;
     public funcEntity(IRType tp,String nm){
         super(tp);
         name = nm;
+        paras = new ArrayList<>();
     }
     @Override
     public String toString(){
-        return "@" + name;
+        StringBuilder ret = new StringBuilder(name + "(");
+        boolean comma=false;
+        for(var para : paras){
+            if (comma) ret.append(", ");
+            ret.append(para.type.toString()).append(" ").append(para.toString());
+        }
+        ret.append(")");
+        return ret.toString();
     }
 }
--- src/IR/instruction/callInst.java
+++ src/IR/instruction/callInst.java
@@ -16,25 +16,17 @@ import java.util.ArrayList;
 public class callInst extends Inst{
     public Register result;
     public funcEntity func;
-    public ArrayList<Entity> paras;
-    public callInst(IRBasicBlock parent, Register result, funcEntity func, ArrayList<Entity> paras){
+    public callInst(IRBasicBlock parent, Register result, funcEntity func){
         super(parent);
         this.result =result;
         this.func = func;
-        this.paras = paras;
     }
     @Override
     public String toString(){
         StringBuilder ret = new StringBuilder();
         if(result != null)
             ret.append(result.toString()).append(" = ");
-        ret.append("call ").append(func.type.toString()).append("(");
-        boolean comma=false;
-        for(var para : paras){
-            if (comma) ret.append(", ");
-            ret.append(para.type.toString()).append(" ").append(para.toString());
-        }
-        ret.append(")");
+        ret.append("call ").append(func.type.toString()).append(func.toString());
         return ret.toString();
     }
 }
